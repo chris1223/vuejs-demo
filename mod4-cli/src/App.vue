48a0097e23590fa460eb0a28b986e964f8f6e6dd
Chris Kao <chris711223@gmail.com>
2020-02-18 15:33:45 +0800

    mod4-4

--- mod4-cli/src/App.vue
+++ mod4-cli/src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container" id="app">
+    <div class="heading">
+      <h1>Posts App</h1>
+      <h6>Sessing all posts.</h6>
+    </div>
+    <posts />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Posts from "./components/Posts.vue";
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
+    Posts
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color:#2c3e50;
+  background-color: rgb(252,233,198);
+  padding:50px
+}
+body{
+  background-color: rgb(252,233,198) !important
 }
 </style>
---
+++ mod4-cli/src/components/Posts.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3>Posts</h3>
+    <b-list-group>
+      <b-list-group-item v-for="p in getAllPosts" v-bind:key="p.id">{{p.title}}</b-list-group-item>
+    </b-list-group>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "Posts",
+  computed: {
+    ...mapGetters(["getAllPosts"])
+  },
+  methods: {
+    ...mapActions(["getPostsAction"])
+  },
+  created() {
+    this.getPostsAction();
+  }
+};
+</script>
+
+<style scoped>
+.list-group-item {
+  color: brown;
+}
+</style>
